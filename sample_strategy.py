d879d7567b3f650a355ceb022b5b083bd37c76f0
hyiiego <hyiiego@qq.com>
2014-10-19 14:57:20 +0800

    read in the graph with status, write final result

--- sample_strategy.py
+++ sample_strategy.py
@@ -6,6 +6,7 @@ def read_graph_with_status(nodes_file, edges_file, status_file):
 	# generate the graph
 	model = DiffusionModel.MultiPlayerLTModel(nodes_file, edges_file, player_num=1)
 
+
 	# store the status of nodes in graph
 	nodes_list = list()
 	with open(status_file,'r') as f:
